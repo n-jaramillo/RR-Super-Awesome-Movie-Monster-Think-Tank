2bc3734c0c9980e5d21b850298ad1e88f3daa00a
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-15 03:14:07 -0700

    adding fallback to Suspense

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Suspense } from 'react';
 import './App.css';
-const AboutUs = React.lazy(() => import('./components/AboutUs'))
-const Packages = React.lazy(() => import('./components/Packages'))
 
 function App() {
+  const AboutUs = React.lazy(() => import('./components/AboutUs'))
+  const Packages = React.lazy(() => import('./components/Packages'))
 
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-        <Suspense>
+        <Suspense fallback={<h1>Loading...</h1>}>
           <AboutUs />
           <Packages />
         </Suspense>
