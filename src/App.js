c647df0878d0db346c406a885a1ec4a7df19988e
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-15 03:12:44 -0700

    refactoring import of components with React.lazy()

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+const AboutUs = React.lazy(() => import('./components/AboutUs'))
+const Packages = React.lazy(() => import('./components/Packages'))
 
 function App() {
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-          <AboutUs />
-          <Packages />
+        <AboutUs />
+        <Packages />
       </div>
     </div>
   );
