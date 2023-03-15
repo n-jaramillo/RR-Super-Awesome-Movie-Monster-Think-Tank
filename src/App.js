7985c551525b0a9db40c5f6cb68f491a0d50dce4
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-15 03:23:42 -0700

    added react-loader-spinner Loader to Suspense in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import './App.css';
+import * as Loader from 'react-loader-spinner'
 
 function App() {
   const AboutUs = React.lazy(() => import('./components/AboutUs'))
@@ -10,7 +11,11 @@ function App() {
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-        <Suspense fallback={<h1>Loading...</h1>}>
+        <Suspense fallback={<Loader.Bars
+          color="#FF00FF"
+          height={200}
+          width={200}
+          style={{ margin: "0 auto" }} />}>
           <AboutUs />
           <Packages />
         </Suspense>
